f2013b6a0dc85d4147e4613de338e05ff2750935
Laurent-Finana <laurentfinanago@gmail.com>
2023-09-15 11:55:46 +0200

    CRUD first calendar

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SlotRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class CalendarController extends AbstractController
 {
     #[Route('/grande-salle', name: 'app_calendar_grande_salle')]
-    public function index(): Response
+    public function index(SlotRepository $slot): Response
     {
-        return $this->render('calendar/index-grande-salle.html.twig', [
-            'controller_name' => 'CalendarController',
-        ]);
+        $events = $slot->findAll();
+
+        $rdvs = [];
+
+        foreach ($events as $event) {
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'title' => $event->getTitle(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'description' => $event->getDescription(),
+                'allDay' => $event->isAllDay(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'textColor' => $event->getTextColor(),
+            ];
+        }
+
+        $data = json_encode($rdvs);
+
+        return $this->render('calendar/index-grande-salle.html.twig', compact('data'));
     }
 }
