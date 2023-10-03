6d14f17e3b976211c4b0a6436feebf957a087eb0
Laurent-Finana <laurentfinanago@gmail.com>
2023-10-03 11:34:27 +0200

    factoring templates twig + update slots in EasyAdmin + add eventClick in FullCalendar

--- src/Controller/Admin/SlotCrudController.php
+++ src/Controller/Admin/SlotCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\Slot;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class SlotCrudController extends AbstractCrudController
 {
@@ -12,14 +20,23 @@ class SlotCrudController extends AbstractCrudController
         return Slot::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            DateTimeField::new('start')->renderAsChoice(),
+            DateTimeField::new('end')->renderAsChoice(),
+            TextareaField::new('description')->hideOnIndex(),
+            BooleanField::new('all_day'),
+            ColorField::new('background_color')->hideOnIndex(),
+            ColorField::new('text_color')->hideOnIndex(),
+            ChoiceField::new('room')->setChoices([
+                'Grand salle' => '1',
+                'Petite salle' => '2',
+                'Musculation' => '3',
+            ])
         ];
     }
-    */
 }
