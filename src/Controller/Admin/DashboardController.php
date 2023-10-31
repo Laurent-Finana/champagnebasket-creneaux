6907a392fe78affae2a54de06352a9fbe6dc2385
Laurent-Finana <laurentfinanago@gmail.com>
2023-10-31 14:01:49 +0100

    Update base.html

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -45,7 +45,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Training Slots');
+            ->setTitle('Administration');
     }
 
     public function configureMenuItems(): iterable
